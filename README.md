71fde278a648a8c250eb147fd432dc024c74d8d5
AlomgirDeveloper <freelanceralomgirofficial@gmail.com>
2023-09-06 02:22:45 +0600

    first commit

--- README.md
+++
@@ -1,1 +0,0 @@
-# Convert-PSD-to-HTML
---
+++ main/js/main.js
@@ -0,0 +1,12 @@
+//NEVIGETION
+function openMenu(){
+   document.getElementById("menu-container").style.width ="100%"; 
+}
+
+function closeMenu(){
+    document.getElementById("menu-container").style.width ="0"; 
+ }
+
+
+//mixiitup
+var mixer = mixitup('.heard-grid')
